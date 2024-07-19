856a4f19ff727c0ed6b963db350846d5bca4c6d1
solidados <geekbrains.pk@internet.ru>
2024-07-19 19:57:42 +0400

    feat: initiate and install cdk for cart service

---
+++ cdk-cart-service/test/cdk-cart-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCartService from '../lib/cdk-cart-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-cart-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCartService.CdkCartServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
